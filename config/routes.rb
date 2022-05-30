60bcceaff251616b578e2f34fdaffc1299a49c9b
Huseyn Hajiyev <hhajiyev@hotmail.com>
2022-05-30 17:08:49 +0300

    added routes and changed cars and user models changed migration files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'cars#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cars, only: %i[index show] do
+    resources :bookings, only: %i[new create]
+  end
 end
--- db/migrate/20220530130543_create_cars.rb
+++ db/migrate/20220530130543_create_cars.rb
@@ -2,7 +2,7 @@ class CreateCars < ActiveRecord::Migration[6.1]
   def change
     create_table :cars do |t|
       t.string :model_name
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :color
 
       t.timestamps
