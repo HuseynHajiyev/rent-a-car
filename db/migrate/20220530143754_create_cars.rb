6168f58acafa913b2f580d197175c27f716ef367
Huseyn Hajiyev <hhajiyev@hotmail.com>
2022-05-30 17:43:53 +0300

    re-migrated, added controller #index and view #index for cars

--- db/migrate/20220530143754_create_cars.rb
+++ db/migrate/20220530143754_create_cars.rb
@@ -1,7 +1,7 @@
 class CreateCars < ActiveRecord::Migration[6.1]
   def change
     create_table :cars do |t|
-      t.string :model_name
+      t.string :car_model
       t.references :user, null: false, foreign_key: true
       t.string :color
 
