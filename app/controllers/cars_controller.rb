2bc62382dc4ebe665a0af0423553735a575b2dc1
Huseyn Hajiyev <hhajiyev@hotmail.com>
2022-05-30 18:18:31 +0300

    added the cars controller and the views for index, new, show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,24 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    @car.save ? render(car_path(@car)) : render(:new)
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_model, :color)
+  end
 end
