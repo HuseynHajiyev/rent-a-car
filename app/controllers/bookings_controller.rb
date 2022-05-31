d22f62b1fc84b6b2b96bcfcc7bbbdc1f20623b6c
Huseyn Hajiyev <hhajiyev@hotmail.com>
2022-05-31 14:10:03 +0300

    completed booking views and controller essentials

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,21 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @car = Car.find(params[:car_id])
   end
 
   def create
-    @booking = booking.new(booking_params)
+    @booking = Booking.new(booking_params)
+    @booking.car = Car.find(params[:car_id])
     @booking.user = current_user
-    @booking.save ? redirect_to(booking_path(@booking)) : render(:new)
+    #@booking.save ? (redirect_to cars_path) : (render :new)
+    @booking.save!
+    redirect_to cars_path
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:booking_model, :color)
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
